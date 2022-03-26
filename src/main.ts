0c23c03fd95b93d6bb580708d329bddd843c2293
Connie Leung <cleung334@gmail.com>
2022-03-26 14:25:01 +0800

    chore: eslint-import-sort but not working

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import * as express from 'express'
 import helmet from 'helmet'
-import { ValidationPipe } from '@nestjs/common'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
